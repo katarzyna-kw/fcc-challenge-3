e8230df4f93e8df05024a16d05e11fa007407997
katarzyna_w <katarzyna.koch@gmail.com>
2021-07-28 11:30:20 -0500

    default styles - mobile

--- js/actions.js
+++ js/actions.js
@@ -4,5 +4,3 @@ let navBarToggle = document.getElementById('js-navbar-toggle');
 navBarToggle.addEventListener('click', function () {
     mainNav.classList.toggle('active');
   });
-
-ScrollReveal().reveal('h-reveal');
