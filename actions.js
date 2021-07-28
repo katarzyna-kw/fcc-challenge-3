4392daac925b00aa06c35ecf0259677b68812c26
katarzyna_w <katarzyna.koch@gmail.com>
2021-07-27 19:04:25 -0500

    refactor/clean up html

--- actions.js
+++
@@ -1,8 +0,0 @@
-let mainNav = document.getElementById("js-menu");
-let toggle = document.getElementById("js-navbar-toggle");
-
-toggle.addEventListener('click', function () {
-    mainNav.classList.toggle('active');
-  });
-
-ScrollReveal().reveal('h-reveal');
