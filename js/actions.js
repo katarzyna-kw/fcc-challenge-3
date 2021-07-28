162b1fe3f28241be71704edbadac72e5914dfae7
katarzyna_w <katarzyna.koch@gmail.com>
2021-07-28 13:37:37 -0500

    hamburger menu closes on anchor link click

--- js/actions.js
+++ js/actions.js
@@ -1,7 +1,15 @@
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
+let anchors = document.querySelectorAll('.anchor')
 
 navBarToggle.addEventListener('click', function () {
     mainNav.classList.toggle('active');
     console.log('clicked')
   });
+
+let length=anchors.length;
+for (let i=0; i<length; i++) {
+  anchors[i].addEventListener('click', function () {
+    mainNav.classList.remove('active');
+  }
+)}
